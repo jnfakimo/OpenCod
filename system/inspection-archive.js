fe290bc9f9bae59ac5821e90a9e7a5c68aa6567f
黃建發 <jnfakimo@gmail.com>
2026-07-15 12:44:59 +0800

    Revert "Temporarily archive inspection system entry points"
    This reverts commit e0bba711859650fd4f89c8ae1656e655adc5de88.

--- system/inspection-archive.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const archivePage = 'inspection-archived.html';
-  if (location.pathname.endsWith('/' + archivePage)) return;
-  const source = location.pathname.split('/').pop() || '巡檢系統';
-  const target = new URL(archivePage, location.href);
-  target.searchParams.set('from', source);
-  location.replace(target.href);
-})();
